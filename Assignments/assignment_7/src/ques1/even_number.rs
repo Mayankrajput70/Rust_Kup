5b16580ee73cc3f085baf571ee70bbbbcc8bcb8d
Mayank <mayank.rajput@knoldus.com>
2021-10-22 23:18:34 +0530

    Adding Assignment_7

--- Assignments/assignment_7/src/ques1/even_number.rs
+++ Assignments/assignment_7/src/ques1/even_number.rs
@@ -1,4 +1,4 @@
-/// number function check the number is "Even and Odd".
+/// number function check the number is "Even".
 ///
 /// #Arguments
 ///
@@ -6,7 +6,7 @@
 ///
 /// #Return
 ///
-/// return : return handle error and use value.
+/// return handle error and use value.
 pub fn number(value: i32) -> Result<String, String> {
     if value % 2 == 0 {
         Ok("Even".to_string())
@@ -22,7 +22,7 @@ pub fn number(value: i32) -> Result<String, String> {
 ///
 /// #Return
 ///
-/// return : return handle error and give output String.
+/// return handle error and give output String.
 pub fn number_test(value: i32) -> String {
     let result = number(value);
 
